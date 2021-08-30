c6296e4b4be27382781cabf3fcd5965f03b811ec
Justin <justin.crippen@gmail.com>
2021-08-30 16:07:03 -0500

    Working on employee Specialties, B6CH6

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState} from "react"
 
 export const EmployeeList = () => {
     const [employees, setEmployees] = useState([]) //returns an array, gives intital value (empty array) (HOOK)
-
+    const [employeeSpecialtyMessage, updateMessage]  = useState("")
     useEffect(
         () => {   //runs code when certain state changes (event listener)
             fetch("http://localhost:8088/employees")
@@ -14,12 +14,24 @@ export const EmployeeList = () => {
         [] //only want this code to run the first time. 
 
     )
+    useEffect(
+        () => { 
+            if (employees.specialty === true) {
+                updateMessage(`${employees.specialty}`)
 
+            } 
+            else {
+                updateMessage(`You have ${employees.length} employees. You will need to make them fight to the death. There can only be one.`)
+            }
+        },
+        [employees]
+    )
 
         return (
             <>
+                <div>{employeeSpecialtyMessage}</div>
                 {
-                    employees.map(
+                    employees.slice(0, 5).map(
                         (employeeObject) => { 
                             return<p key={`employee--${employeeObject.id}`}>{employeeObject.name}</p>
                         }
--- src/components/customers/CustomerList.js
+++ src/components/customers/CustomerList.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState} from "react"
 
 export const CustomerList = () => {
     const [customers, setCustomers] = useState([]) //returns an array, gives intital value (empty array) (HOOK)
-
+    const [totalCustomerMessage, updateMessage] = useState("")
     useEffect(
         () => {   //runs code when certain state changes (event listener)
             fetch("http://localhost:8088/customers")
@@ -14,12 +14,24 @@ export const CustomerList = () => {
         [] //only want this code to run the first time. 
 
     )
+    useEffect(
+        () => { 
+            if (customers.length === 1) {
+                updateMessage(`You have 1 customer`)
 
+            } 
+            else {
+                updateMessage(`You have a total of ${customers.length} customers`)
+            }
+        },
+        [customers]
+    )
 
         return (
             <>
+                <div>{totalCustomerMessage}</div>
                 {
-                    customers.map(
+                    customers.slice(0, 5).map(
                         (customerObject) => {
                             return<p key={`customer--${customerObject.id}`}>{customerObject.name}</p>
                         }
