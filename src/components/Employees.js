7ad4637b518b751e0d1d66729afe4b22cc78bd50
Justin <justin.crippen@gmail.com>
2021-09-02 11:46:05 -0500

    Complete BK6CH6

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,37 +1,32 @@
-import React, { useEffect, useState} from "react"
+import React, { useEffect, useState } from "react"
 
 export const EmployeeList = () => {
-    const [employees, setEmployees] = useState([]) //returns an array, gives intital value (empty array) (HOOK)
-    const [employeeSpecialtyMessage, updateMessage]  = useState("")
+    const [employees, changeEmployee] = useState([])
+    const [specialties, setSpecial] = useState("")
+
     useEffect(
-        () => {   //runs code when certain state changes (event listener)
+        () => {
             fetch("http://localhost:8088/employees")
                 .then(res => res.json())
-                .then((employeeArray) => {
-                    setEmployees(employeeArray)
+                .then((employeesFromAPI) => {
+                    changeEmployee(employeesFromAPI)
                 })
-        }, //runs code when certain state changes (event listener)
-        [] //only want this code to run the first time. 
-
-    )
-    useEffect(
-        () => { 
-            if (employees.specialty === true) {
-                updateMessage(`${employees.specialty}`)
-
-            } 
-            else {
-                updateMessage(`You have ${employees.length} employees. You will need to make them fight to the death. There can only be one.`)
-            }
         },
-        [employees]
+        []
     )
+//event listener, only watching employee state
+    useEffect(() => {
+        const justSpecialities = employees.map(emp => emp.specialty)
+        setSpecial(justSpecialities.join(", "))
+    }, [employees])//watching data change in employees
+
+    
 
         return (
             <>
-                <div>{employeeSpecialtyMessage}</div>
+                <div>Specialties: { specialties }</div>
                 {
-                    employees.slice(0, 5).map(
+                    employees.map(
                         (employeeObject) => { 
                             return<p key={`employee--${employeeObject.id}`}>{employeeObject.name}</p>
                         }
